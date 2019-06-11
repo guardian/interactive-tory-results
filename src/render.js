e215dea963129a3562213e0f61367c217e196ffc
Seán Clarke <sean.clarke@guardian.co.uk>
2019-06-11 11:53:41 +0100

    better data processing, more styles

--- src/render.js
+++ src/render.js
@@ -3,13 +3,36 @@ import mustache from 'mustache'
 import axios from 'axios'
 import _ from 'lodash'
 
-const dataurl = 'https://interactive.guim.co.uk/docsdata-test/1blisaXnBVa7HdR6iq6hEEQJjQ2B8-hEsd6dH_WrPfS0.json'
+const dataurl = 'https://interactive.guim.co.uk/docsdata-test/1blisaXnBVa7HdR6iq6hEEQJjQ2B8-hEsd6dH_WrPfS0.json';
+
+async function getGlosses(rounds,data) {
+
+    var glosses = data.rounds;
+
+    rounds = rounds.map(r => {
+
+        var matchinggloss = glosses.find(g => {
+            return g.round == r.round
+        })
+
+        if (typeof matchinggloss != "undefined") {
+
+
+            for (var prop in matchinggloss) {
+                r[prop] = matchinggloss[prop]
+            }
+
+            return r;
+
+        }
+    })
+    return rounds;
+}
 
 async function parse(data) {
-
-    data = _(data).groupBy('round').values()
-    var rounds = {}
-    data.forEach(r => {
+    let votedata = _(data.votes).groupBy('round').values();
+    let rounds = [];
+    votedata.forEach(r => {
         r.sort((a,b) => {
             return b.votes - a.votes;
         });
@@ -17,19 +40,23 @@ async function parse(data) {
         r.map(c => {
             c.width = `${100 * (c.votes / leader.votes)}%`
             return c;
-        })
-        rounds[r[0].round] = r
+        });
+        rounds.push({
+            'round' : r[0].round,
+            'results' : r
+        });
         return r;
-    })
+    });
+    rounds = await getGlosses(rounds,data);
     console.log(rounds)
-    return rounds;
 
-}
+    return rounds;
+};
 
 
 export async function render() {
     var rawdata = (await axios.get(dataurl)).data;
-    var data = await parse(rawdata.sheets.Sheet1);
+    var data = await parse(rawdata.sheets);
     var output = mustache.render(templateHTML,data)
     return output;
 }
